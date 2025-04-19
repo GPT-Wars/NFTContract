082283333bb237a5da6d2a3676c1557cdfb3c823
SnakeEyes <ghofrane.imran@gmail.com>
2025-04-19 04:41:02 +0200

    Initial Commit v1.0

---
+++ truffle-config.js
@@ -0,0 +1,36 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const path = require("path");
+
+const mnemonic =
+  "artefact muscle now club answer ask vendor okay loop believe party amazing";
+
+module.exports = {
+  contracts_build_directory: path.join(__dirname, "/contracts"),
+  networks: {
+    development: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: { phrase: mnemonic },
+          providerOrUrl: "http://127.0.0.1:7545",
+          numberOfAddresses: 10,
+        }),
+      network_id: "*",
+      gas: 6721975, // Augmenter si nécessaire
+      gasPrice: 20000000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.13",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+  db: {
+    enabled: false,
+  },
+};
